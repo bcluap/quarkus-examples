0405cea0f5ba86ec24aedf0d2456b5d51827c3e5
Paul Carter-Brown <paul.carter-brown@jini.guru>
2021-01-24 14:10:10 +0200

    Filter example

--- resteasy-reactive/src/main/java/reactive/scenarios/RestResource.java
+++ resteasy-reactive/src/main/java/reactive/scenarios/RestResource.java
@@ -5,19 +5,14 @@
  */
 package reactive.scenarios;
 
-import io.smallrye.common.annotation.Blocking;
-import java.math.BigDecimal;
-import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -27,65 +22,16 @@ import javax.ws.rs.core.MediaType;
 @Path("/")
 public class RestResource {
 
-    @Inject
-    RestSubResource restSubResource;
+     private static final Logger log = LoggerFactory.getLogger(RestResource.class);
+     
 
-
-    /*
-    NOTE: The name parameter is not bound in the sub resource when using reactive mode
-    */
-    @Path("sub-resource/{name}")
-    public RestSubResource getSubResource(@PathParam("name") String name) {
-        return restSubResource;
-    }
-
-    /*
-    NOTE: In reactive mode this method will cause the build will fail
-    Build step io.quarkus.resteasy.reactive.server.deployment.ResteasyReactiveProcessor#setupEndpoints threw an exception: java.lang.RuntimeException: java.lang.RuntimeException: 
-    Failed to process method reactive.scenarios.RestResource#java.lang.String doesNotBuildInReactiveMode(java.util.List<reactive.scenarios.SomeClass> inList)
-     */
-    
     @POST
-    @Path("/listofsomeclass")
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/test")
+    @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
-    public String doesNotBuildInReactiveMode(List<SomeClass> inList) {
-        return "OK";
-    }
-
-    /*
-    NOTE: In reactive mode Quarkus will throw an error when booting
-    Caused by: java.lang.ClassNotFoundException: java.math.BigDecimal$quarkusrestparamConverter$
-     */
-
-    
-    @GET
-    @Path("/bigdecimal/{x}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.TEXT_PLAIN)
-    public BigDecimal doesNotBootInReactiveMode(@PathParam("x") BigDecimal x) {
-        return x;
-    }
-    
-    
-    /*
-    NOTE: This returns a string with escaped JSON in reactive mode
-    */
-    @POST
-    @Path("/stringarray")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<String> returnsStringInReactiveMode(List<String> test) {
-        return test;
-    }
-
-    @GET
-    @Blocking
-    @Path("/dummypath")
-    @Consumes(MediaType.WILDCARD)
-    @Produces(MediaType.WILDCARD)
-    public String dummy() {
-        return "OK";
+    public String go(String s) {
+        log.warn("Body is [{}]", s);
+        return s;
     }
 
 }
--- resteasy-reactive/src/test/java/reactive/scenarios/ReactiveScenariosTest.java
+++ resteasy-reactive/src/test/java/reactive/scenarios/ReactiveScenariosTest.java
@@ -4,6 +4,7 @@ import static io.restassured.RestAssured.given;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import io.smallrye.common.annotation.Blocking;
 import static org.hamcrest.CoreMatchers.is;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -15,50 +16,14 @@ public class ReactiveScenariosTest {
     private static final Logger log = LoggerFactory.getLogger(ReactiveScenariosTest.class);
 
     @Test
-    public void testSubResorceBinding() {
-        given()
-                .get("sub-resource/abc")
-                .then()
-                .statusCode(200)
-                .body(is("abc"));
-    }
-
-    @Test
     public void testListOfSomeClass() {
-        given().contentType(ContentType.JSON)
-                .body("[]")
-                .post("/listofsomeclass")
+        given().contentType(ContentType.TEXT)
+                .body("HELLO")
+                .post("/test")
                 .then()
                 .statusCode(200)
-                .body(is("OK"));
+                .body(is("HELLO"));
     }
 
-    @Test
-    public void testBigDecimalPathParam() {
-        given().get("/bigdecimal/1.1")
-                .then()
-                .statusCode(200)
-                .body(is("1.1"));
-    }
-
-    @Test
-    public void testPostingStringArray() {
-        given().contentType(ContentType.JSON)
-                .accept(ContentType.JSON)
-                .body("[\"a\",\"b\"]")
-                .post("/stringarray")
-                .then()
-                .statusCode(200)
-                .body(is("[\"a\",\"b\"]"));
-    }
-
-    @Test
-    public void testFilterAbortingWithByteArray() {
-        given()
-                .header("prematchresponse", "yes")
-                .get("/dummypath")
-                .then()
-                .statusCode(200).contentType(ContentType.BINARY);
-    }
 
 }
