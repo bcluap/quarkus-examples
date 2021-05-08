c2ba4f23915554631968b8b4ec2b320309596773
Paul Carter-Brown <paul.carter-brown@jini.guru>
2021-05-08 12:33:26 +0200

    MultiModule

---
+++ multi-module/mm-1/src/main/java/rest1/Rest1.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) Jini Guru Technologies (Mauritius) Limited - Company No. : 154309 - All Rights Reserved Unauthorized copying of
+ * this file or any of its contents, via any medium is strictly prohibited Proprietary and confidential
+ */
+package rest1;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ApplicationScoped
+@Path("/get1")
+public class Rest1 {
+
+    private static final Logger log = LoggerFactory.getLogger(Rest1.class);
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String get1() {
+        return "Hello world";
+    }
+
+}
